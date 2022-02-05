2148b0fa6fa60395555f581df4aad37930b0260f
Chlorokin <chlorokin@fastmail.com>
2022-02-05 07:54:12 -0800

    moved console to own file

---
+++ Publish_and_Perish_files/console.js
@@ -0,0 +1,13 @@
+async function typeWriter(txt) {
+        for (let i = 0; i < txt.length; i++) {
+            document.getElementById("console").innerHTML += txt.charAt(i);
+            await sleep(1);
+        }
+    return 1;
+}
+
+async function  console_add_text(txt){
+    document.getElementById("publish_paper").style.display = "none";
+    await typeWriter(txt);
+    document.getElementById("publish_paper").style.display = "inline";
+}
--- Publish_and_Perish_files/main.js
+++ Publish_and_Perish_files/main.js
@@ -1,3 +1,6 @@
+
+
+
 function main(){
     document.getElementById('publish_paper').addEventListener('click', publishPaper);
 }
@@ -6,21 +9,6 @@ function main(){
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-
-async function typeWriter(txt) {
-        for (let i = 0; i < txt.length; i++) {
-            document.getElementById("console").innerHTML += txt.charAt(i);
-            await sleep(1);
-        }
-    return 1;
-}
-
-async function  console_add_text(txt){
-    document.getElementById("publish_paper").style.display = "none";
-    await typeWriter(txt);
-    document.getElementById("publish_paper").style.display = "inline";
-}
-
 
 function publishPaper() {
     document.getElementById("console").innerHTML = "";
