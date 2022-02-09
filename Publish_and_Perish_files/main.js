e2f1faae4093c8bb432a05cabc38e5e04be382b0
Chlorokin <chlorokin@fastmail.com>
2022-02-08 21:14:54 -0800

    Added time, tabs in html

--- Publish_and_Perish_files/main.js
+++ Publish_and_Perish_files/main.js
@@ -1,9 +1,42 @@
+var time_is_a_global_variable = Math.round(+new Date()/1000);
+var pause_time_is_a_global_variable = false
+
+function startTime(){
+    pause_time_is_a_global_variable = false;
+    }
+
+function stopTime(){
+    pause_time_is_a_global_variable = true;
+    }
+
+setInterval(function(){
+    if (pause_time_is_a_global_variable == false)
+    time_is_a_global_variable += 60 
+    let date_object = new Date(time_is_a_global_variable * 1000);
+    //Now we get the month, day, and year from the date_object
+    let month = date_object.getUTCMonth() + 1; //months from 1-12
+    let day = date_object.getUTCDate();
+    let year = date_object.getUTCFullYear();
+    //Now we make a nice formatted string for the month, day, and year
+    let new_date = month + "/" + day + "/" + year;
+    // now we get the hour and minutes from the date_object
+    let hour = date_object.getUTCHours();
+    if (hour < 10){
+        hour = "0" + hour;
+    }   
+    let minute = date_object.getUTCMinutes();
+    if (minute < 10){
+        minute = "0" + minute;
+    }
+    let clock_string = hour + ":" + minute;
+
+    document.getElementById("date_display").innerHTML = new_date + " " + clock_string;
+ }, 10);
 
 function renderState(game_state){
-    let total_papers = game_state.papers_published.list_of_papers.length;
-    document.getElementById("num_papers").innerHTML = total_papers;
-    let num_cites = game_state.num_cites;
-    document.getElementById("num_cites").innerHTML = num_cites;
+    document.getElementById("num_papers").innerHTML = game_state.papers_published.list_of_papers.length;
+    document.getElementById("num_cites").innerHTML = game_state.num_cites;
+    game_state.time_step++;
 }
 
 
@@ -13,6 +46,8 @@ async function main(){
     game_state.level = 0;
     game_state.num_cites = 0;
     game_state.papers_published = {list_of_papers:[]};
+    game_state.console_window_array = [];
+    game_state.time_step = 0
     //let papers = await generateNodeList();
     //papers = papers.sort(function(a,b){
     //    return b.degree - a.degree;
